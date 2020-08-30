754927306ab00d0433e65ad2cc2b22669977763e
rishirajjain <rishirajjain319@gmail.com>
2020-08-31 03:58:23 +0530

    redesign

---
+++ dist/_nuxt/static/1598826336/blog/user-interface-design-guidelines/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1598826336",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},theme:{}},serverRendered:true,routePath:"\u002Fblog\u002Fuser-interface-design-guidelines",config:{}};
