162c827283b32c8c0ef373e2399fd64393c45546
rishirajjain <rishirajjain319@gmail.com>
2020-09-28 03:05:33 +0530

    modifications and improvements

---
+++ dist/_nuxt/static/1601242467/work/bharatexotics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1601242467",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fwork\u002Fbharatexotics",config:{}};
