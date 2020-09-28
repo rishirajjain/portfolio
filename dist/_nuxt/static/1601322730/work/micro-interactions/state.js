8c21ea90b9722213c79380060eec674d36d7919f
rishirajjain <rishirajjain319@gmail.com>
2020-09-29 02:08:03 +0530

    modifications

---
+++ dist/_nuxt/static/1601322730/work/micro-interactions/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1601322730",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fwork\u002Fmicro-interactions",config:{}};
