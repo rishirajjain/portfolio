cad6f6dd4b417e38e70d50b1af3dc0b65ca50dde
Rishi Raj Jain <rishirajjain319@gmail.com>
2021-06-21 19:27:30 +0530

    font size fix

---
+++ dist/_nuxt/static/1624283817/blog/how-qpr-helps-in-suicide-prevention/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624283817",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fblog\u002Fhow-qpr-helps-in-suicide-prevention",config:{}};
