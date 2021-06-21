8204c27ef26a71a1295f146312c603bda7f83c63
Rishi Raj Jain <rishirajjain319@gmail.com>
2021-06-21 18:32:47 +0530

    about changes

---
+++ dist/_nuxt/static/1624280518/work/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624280518",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:false,routePath:a,config:{}}}("\u002Fwork"));
