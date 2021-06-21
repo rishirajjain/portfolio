b1ea20f6d9407bd0c74dbac34459e25c99e2afb9
Rishi Raj Jain <rishirajjain319@gmail.com>
2021-06-21 18:40:34 +0530

    fix about bug

---
+++ dist/_nuxt/static/1624280992/work/portfolio-version-3/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624280992",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fwork\u002Fportfolio-version-3",config:{}};
