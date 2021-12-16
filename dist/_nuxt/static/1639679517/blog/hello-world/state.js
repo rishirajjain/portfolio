8f892700c8c246efae6b324beca91f72cf131bc4
Rishi Raj Jain <rishirajjain319@gmail.com>
2021-12-17 00:06:19 +0530

    improvements

---
+++ dist/_nuxt/static/1639679517/blog/hello-world/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639679517",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fblog\u002Fhello-world",config:{}};
