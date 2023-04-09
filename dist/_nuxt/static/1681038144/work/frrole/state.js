f2b2c58cff699d50bb3e1ef6057929cd92099ded
Rishi Raj Jain <rishirajjain319@gmail.com>
2023-04-09 16:42:13 +0530

    Updated ui

---
+++ dist/_nuxt/static/1681038144/work/frrole/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681038144",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fwork\u002Ffrrole",config:{}};
