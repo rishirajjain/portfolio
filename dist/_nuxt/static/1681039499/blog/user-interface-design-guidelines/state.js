37060572ac19119bb859f600d77dc095c6ea9834
Rishi Raj Jain <rishirajjain319@gmail.com>
2023-04-09 16:57:42 +0530

    bug squashed

---
+++ dist/_nuxt/static/1681039499/blog/user-interface-design-guidelines/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681039499",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fblog\u002Fuser-interface-design-guidelines",config:{}};
