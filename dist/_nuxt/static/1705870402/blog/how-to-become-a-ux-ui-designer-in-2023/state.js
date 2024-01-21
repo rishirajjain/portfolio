e8ceac3b44db5118a5ef0ad420b87f513f5119bc
Rishi Raj Jain <rishirajjain319@gmail.com>
2024-01-22 02:26:43 +0530

    updates loader and minor improvements

---
+++ dist/_nuxt/static/1705870402/blog/how-to-become-a-ux-ui-designer-in-2023/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1705870402",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fblog\u002Fhow-to-become-a-ux-ui-designer-in-2023",config:{}};
---
+++ dist/_nuxt/static/1705870402/work/bharatexotics/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1705870402",layout:"default",error:null,state:{workData:{},workDataIndi:{},blogData:{},blogDataIndi:{},gallery:{},theme:{}},serverRendered:true,routePath:"\u002Fwork\u002Fbharatexotics",config:{}};
